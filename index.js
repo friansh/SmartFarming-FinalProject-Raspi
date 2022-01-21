20b2ce1a1787afd974194c3d68e41d5bb148713a
Fikri Rida Pebriansyah <fikriultimate18@gmail.com>
2022-01-22 01:19:34 +0700

    Developing...

--- index.js
+++ index.js
@@ -17,36 +17,111 @@ printWithTimestamp(
 );
 
 const Axios = require("axios");
-Axios.defaults.baseURL = process.env.DEVICE_API_URL;
 
-let configuration = {};
+let obtainSensorsDataIntervalId;
+let gatherConfigIntervalId;
 
 gatherConfiguration();
 
+let lastReceivedConfig = 0;
+
+var querystring = require("querystring");
+
 async function gatherConfiguration() {
-  configuration = await Axios.post(
-    `${process.env.API_URL}/agroclimate/device`,
-    {
-      device_token: process.env.DEVICE_TOKEN,
-    }
-  ).catch((err) => {
-    printWithTimestamp(
-      `oo[CONF] Failed getting the agroclimate configuration from server. Error code: ${err.response.status} (${err.response.data}).`
-    );
-  });
+  Axios.post(`${process.env.API_URL}/agroclimate/device`, {
+    device_token: process.env.DEVICE_TOKEN,
+  })
+    .then((response) => {
+      const configReceivedTime = Date.now();
+      const config = response.data;
+      // console.log(config);
 
-  if (configuration == undefined) return;
+      printWithTimestamp(
+        `oo[CONF] The configuration obtained from the server successfully.`
+      );
+      printWithTimestamp(
+        `oo[CONF] The configuration from server last update at: ${new Date(
+          config.updatedAt
+        ).getTime()} and received at: ${configReceivedTime}. So, the latency is ${
+          configReceivedTime -
+          new Date(config.updatedAt).getTime() -
+          config.refresh_time -
+          lastReceivedConfig
+        } ms`
+      );
 
-  console.log(configuration.data);
+      lastReceivedConfig =
+        configReceivedTime - new Date(config.updatedAt).getTime();
 
-  printWithTimestamp(
-    "oo[CONF] Done getting the agroclimate configuration from server."
-  );
+      Axios.post(
+        `${process.env.DEVICE_API_URL}/config`,
+        querystring.stringify({
+          ph: config.ph,
+          light_intensity: config.light_intensity,
+          nutrient_flow: config.nutrient_flow,
+        }),
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+        }
+      )
+        .then((response) => {
+          printWithTimestamp(
+            `oo[CONF] The new configuration has sent to Arduino successfully.`
+          );
+          console.log(response.data);
+        })
+        .catch((err) => {
+          printWithTimestamp(
+            `oo[CONF] Failed to set the new configuration to Arduino. Error code: ${err.response.status} (${err.response.data}).`
+          );
+        });
+
+      if (gatherConfigIntervalId != undefined)
+        clearInterval(gatherConfigIntervalId);
+      gatherConfiguration;
+
+      gatherConfigIntervalId = setInterval(
+        gatherConfiguration,
+        config.refresh_time
+      );
+
+      if (obtainSensorsDataIntervalId != undefined)
+        clearInterval(obtainSensorsDataIntervalId);
+
+      obtainSensorsDataIntervalId = setInterval(() => {
+        printWithTimestamp(
+          "oo[HTTP] Obtaining the sensors data from Arduino..."
+        );
+        Axios.get(`${process.env.DEVICE_API_URL}/sensors`)
+          .then((response) => {
+            let dataRoutine = {
+              ph: response.data.ph,
+              light_intensity: response.data.light_intensity,
+              nutrient_flow: response.data.nutrient_flow,
+              tds: response.data.tds,
+              ec: response.data.ec,
+              sent: Date.now(),
+            };
+
+            printWithTimestamp("oo[MQTT] Transmitting data to the server...");
+            mqttClient.publish(
+              `smartfarmer/data/${process.env.DEVICE_TOKEN}`,
+              JSON.stringify(dataRoutine)
+            );
+          })
+          .catch((error) => {
+            console.log(error.response);
+          });
+      }, parseInt(config.logging_time));
+    })
+    .catch((err) => {
+      printWithTimestamp(
+        `oo[CONF] Failed getting the agroclimate configuration from server. Error code: ${err.response.status} (${err.response.data}).`
+      );
+    });
 }
-
-const clientCert = fs.readFileSync("./certs/smartfarmer-client-raspi.cert.pem");
-const clientKey = fs.readFileSync("./certs/smartfarmer-client-raspi.key");
-const caFile = fs.readFileSync("./certs/root-ca.cert.pem");
 
 printWithTimestamp("oo[BOOT] Done loading the required certificates.");
 
@@ -78,30 +153,6 @@ mqttClient.on("message", function (topic, message) {
 
   printWithTimestamp(status);
 });
-
-setInterval(() => {
-  printWithTimestamp("oo[SERL] Obtaining the sensors data from Arduino...");
-  Axios.get("/sensors")
-    .then((response) => {
-      let dataRoutine = {
-        ph: response.data.ph,
-        light_intensity: response.data.light_intensity,
-        nutrient_flow: response.data.nutrient_flow,
-        tds: response.data.tds,
-        ec: response.data.ec,
-        sent: Date.now(),
-      };
-
-      printWithTimestamp("oo[MQTT] Transmitting data to the server...");
-      mqttClient.publish(
-        `smartfarmer/data/${process.env.DEVICE_TOKEN}`,
-        JSON.stringify(dataRoutine)
-      );
-    })
-    .catch((error) => {
-      console.log(error.response);
-    });
-}, parseInt(process.env.SENSOR_OBTAINING_INTERVAL));
 
 /*
   console.log(data);
